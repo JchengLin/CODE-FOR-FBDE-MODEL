75da1ccb9a788697d80fbb9351de5692d0cefa31
jch_lin <1027930777@qq.com>
2022-01-07 00:04:48 +0800

    Update FID_IS.py

--- EVALUATION PROTOCAL/FID_IS.py
+++ EVALUATION PROTOCAL/FID_IS.py
@@ -1,1 +1,12 @@
 
+pip install torch-fidelity
+
+# IS
+fidelity --gpu 0 --isc --input1 img_dir1/
+
+#FID
+fidelity --gpu 0 --fid --input1 img_dir1/ --input2 img_dir2/
+
+#KID
+fidelity --gpu 0 --kid --input1 img_dir1/ --input2 img_dir2/
+
